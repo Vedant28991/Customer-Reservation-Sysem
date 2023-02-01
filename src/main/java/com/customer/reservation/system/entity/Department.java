8027545d46653507999ab457d18a348110284a85
KL34TG <darshan.h.kk.ext@ing.com>
2023-02-01 15:23:16 +0530

    resources

--- src/main/java/com/customer/reservation/system/entity/Department.java
+++ src/main/java/com/customer/reservation/system/entity/Department.java
@@ -1,7 +1,15 @@
 package com.customer.reservation.system.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Department {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer departmentId;
 	private String departmnetName;
 	
